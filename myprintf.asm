44430cdeda337823aab9f0e3b612897cd3525dbf
Arseniy <arseniy@neburalis.space>
2026-03-26 06:39:41 +0000

    Major changes

--- myprintf.asm
+++ myprintf.asm
@@ -11,10 +11,14 @@ section .text
     global putbin
     global putchar
     global putstr
+
     %include "linux64_macro.asm"
 
+; =============================================================================
 ; void puthex(uint64_t a);
-;   prints the number in hexadecimal to stdout using Linux write syscall.
+; =============================================================================
+; prints the number in hexadecimal to stdout using Linux write syscall.
+;   rdi = number
 puthex:
     sub rsp, 24
     mov rdx, rdi
@@ -52,8 +56,11 @@ puthex:
     add rsp, 24
     ret
 
+; =============================================================================
 ; void putuint(uint64_t a);
-;   prints unsigned integer in decimal to stdout.
+; =============================================================================
+; prints unsigned integer in decimal to stdout.
+;   rdi = number
 putuint:
     sub rsp, 40
     mov rax, rdi
@@ -89,8 +96,11 @@ putuint:
     add rsp, 40
     ret
 
+; =============================================================================
 ; void putint(int64_t a);
-;   prints signed integer in decimal to stdout.
+; =============================================================================
+; prints signed integer in decimal to stdout.
+;   rdi = number
 putint:
     sub rsp, 24
     test rdi, rdi
@@ -114,8 +124,18 @@ putint:
     add rsp, 24
     ret
 
+section .data
+
+Int64MinValue: db "9223372036854775808"
+Int64MinValueLen equ $ - Int64MinValue
+
+section .text
+
+; =============================================================================
 ; void putoct(uint64_t a);
-;   prints number in octal to stdout.
+; =============================================================================
+; prints number in octal to stdout.
+;   rdi = number to print
 putoct:
     sub rsp, 40
     mov rax, rdi
@@ -149,8 +169,11 @@ putoct:
     add rsp, 40
     ret
 
+; =============================================================================
 ; void putbin(uint64_t a);
-;   prints number in binary to stdout.
+; =============================================================================
+; prints number in binary to stdout.
+;   rdi = number
 putbin:
     sub rsp, 80
     mov rax, rdi
@@ -184,8 +207,11 @@ putbin:
     add rsp, 80
     ret
 
+; =============================================================================
 ; void putchar(char a);
-;   prints one byte to stdout.
+; =============================================================================
+; prints one byte to stdout.
+;   dil = ch to print
 putchar:
     sub rsp, 16
     mov byte [rsp], dil
@@ -193,8 +219,11 @@ putchar:
     add rsp, 16
     ret
 
+; =============================================================================
 ; void putstr(char* str);
-;   computes string length and writes entire string using one syscall.
+; =============================================================================
+; computes string length and writes entire string using one syscall.
+;   rdi -> str
 putstr:
     test rdi, rdi
     jz .empty
@@ -215,21 +244,258 @@ putstr:
 .empty:
     ret
 
+; =============================================================================
+; JmpTable:
+section .rodata
+align 8
+
+; Индекс: (*fmt - 'b')
+; Диапазон: 'b'..'x'
+jmp_table:
+    dq case_b        ; 'b'
+    dq case_c        ; 'c'
+    dq case_d        ; 'd'
+    dq case_default  ; 'e'
+    dq case_default  ; 'f'
+    dq case_default  ; 'g'
+    dq case_default  ; 'h'
+    dq case_default  ; 'i'
+    dq case_default  ; 'j'
+    dq case_default  ; 'k'
+    dq case_default  ; 'l'
+    dq case_default  ; 'm'
+    dq case_default  ; 'n'
+    dq case_o        ; 'o'
+    dq case_default  ; 'p'
+    dq case_default  ; 'q'
+    dq case_default  ; 'r'
+    dq case_s        ; 's'
+    dq case_default  ; 't'
+    dq case_u        ; 'u'
+    dq case_default  ; 'v'
+    dq case_default  ; 'w'
+    dq case_x        ; 'x'
+
+section .text
+
+; =============================================================================
+; int32_t printer(char *fmt, uint8_t *ptr1, uint8_t *ptr2)
+; =============================================================================
+; calling convention:
+;   rdi -> fmt
+;   rsi -> ptr_to_arg1..5
+;   rdx -> ptr_to_arg6...
+;   who pushed - that popped (calling func need to pop all )
+; Почему так?
+; Для более удобного написания трамплина для вызова по ABI. Потому что первые 6 аргументов (без fmt получается 5)
+; лежат в регистрах, а последующие в стеке (под адресом возврата). Поэтому трамплин пушит 5 аргументов в стек, и
+; передает 2 указателя: на 1й аргумент (последний запушенный в стек) и на первый элемент стека трамплина
+;
+; Структура стека:
+;                  ptr1                                                                   ptr2
+;                   ↓                                                                      ↓
+; | BP | RetAddr | %1 | %2 | %3 | %4 | %5 | aligning | chain jmp BP | chain jmp RetAddr | %6 | ...
+;
+; Псевдокод логики функции:
+; int32_t printer(char *fmt, uint8_t *ptr1, uint8_t *ptr2) {
+;
+;     int32_t arg_count = 0;
+;
+;     while (*fmt != '\0') {
+;         if (*fmt != '%') {
+;             putchar(*fmt);
+;             ++fmt;
+;             continue;
+;         }
+;
+;         ++fmt;
+;
+;         if (*fmt == '\0') {
+;             // UB
+;             break;
+;         }
+;
+;         if (*fmt == '%') {
+;             putchar('%');
+;             ++fmt;
+;             continue;
+;         }
+;
+;         uint8_t *arg_ptr;
+;         if (arg_count < 5)
+;             arg_ptr = ptr1 + arg_count * 8;
+;         else
+;             arg_ptr = ptr2 + (arg_count - 5) * 8;
+;
+;         switch (*fmt) {
+;             case 'b': putbin(*arg_ptr);  break;
+;             case 'c': putchar(*arg_ptr); break;
+;             case 'd': putint(*arg_ptr);  break;
+;             case 'o': putoct(*arg_ptr);  break;
+;             case 's': putstr(arg_ptr);   break;
+;             case 'u': putuint(*arg_ptr); break;
+;             case 'x': puthex(*arg_ptr);  break;
+;             default:
+;                 // UB
+;                 break;
+;         }
+;
+;         ++arg_count;
+;         ++fmt;
+;     }
+; }
+;
+printer:
+    push rbp
+    mov  rbp, rsp
+
+    ; используем callee-saved регистры
+    push rbx
+    push r12
+    push r13
+    push r14
+
+    ; r12 = fmt
+    ; r13 = ptr1
+    ; r14 = ptr2
+    ; ebx = arg_count
+    mov  r12, rdi
+    mov  r13, rsi
+    mov  r14, rdx
+    xor  ebx, ebx
+
+.loop:
+    ; while (*fmt != '\0')
+    movzx rax, byte [r12]
+    test  al, al
+    je    .done
+
+    ; if (*fmt != '%')
+    cmp   al, '%'
+    je    .percent
+
+    ; putchar(*fmt)
+    movzx rdi, byte [r12]
+    call  putchar
+
+    ; ++fmt
+    inc   r12
+    jmp   .loop
+
+.percent:
+    ; ++fmt
+    inc   r12
+
+    ; if (*fmt == '\0') // UB break;
+    movzx rax, byte [r12]
+    test  al, al
+    je    .done
+
+    ; if (*fmt == '%') { putchar('%'); ++fmt; continue; }
+    cmp   al, '%'
+    jne   .select_arg
+
+    mov   rdi, '%'
+    call  putchar
+    inc   r12
+    jmp   .loop
+
+.select_arg:
+    ; if (arg_count < 5)
+    ;     arg_ptr = ptr1 + arg_count * 8;
+    ; else
+    ;     arg_ptr = ptr2 + (arg_count - 5) * 8;
+    cmp   ebx, 5
+    jl    .from_ptr1
+
+    movzx rax, ebx
+    sub   rax, 5
+    shl   rax, 3
+    lea   rdi, [r14 + rax]
+    jmp   .dispatch
+
+.from_ptr1:
+    movzx rax, ebx
+    shl   rax, 3
+    lea   rdi, [r13 + rax]
+
+.dispatch:
+    ; eax = *fmt
+    movzx rax, byte [r12]
+
+    ; Нормализуем индекс: 'b' -> 0
+    sub   rax, 'b'
+    cmp   rax, ('x' - 'b')
+    ja    case_default
+
+    mov   rax, [rel jmp_table + rax*8]
+    jmp   rax
+
+case_b:
+    ; putbin(arg_ptr)
+    call  putbin
+    jmp   printer.after_spec
+
+case_c:
+    ; putchar(*arg_ptr)
+    movzx dil, byte [rdi]
+    call  putchar
+    jmp   printer.after_spec
+
+case_d:
+    ; putint(*arg_ptr)
+    movzx rdi, byte [rdi]
+    call  putint
+    jmp   printer.after_spec
+
+case_o:
+    ; putoct(*arg_ptr)
+    movzx rdi, byte [rdi]
+    call  putoct
+    jmp   printer.after_spec
+
+case_s:
+    ; putstr(arg_ptr)
+    call  putstr
+    jmp   printer.after_spec
+
+case_u:
+    ; putuint(*arg_ptr)
+    movzx rdi, byte [rdi]
+    call  putuint
+    jmp   printer.after_spec
+
+case_x:
+    ; puthex(*arg_ptr)
+    movzx rdi, byte [rdi]
+    call  puthex
+    jmp   printer.after_spec
+
+case_default:
+    ; UB -> просто выходим
+    jmp   printer.done
+
+printer.after_spec:
+    ; ++arg_count
+    inc   ebx
+
+    ; ++fmt
+    inc   r12
+    jmp   printer.loop
+
+printer.done:
+    xor   rax, rax
+
+    pop   r14
+    pop   r13
+    pop   r12
+    pop   rbx
+    pop   rbp
+    ret
+
 ; ============= Program entry ================================================
 _start:
-    write_no_pie 1, Msg, MsgLen
-    
-    mov rdi, 'C'
-    call putchar
-
-    mov rdi, 0x0A
-    call putchar
-
-    mov rdi, TestPutstr
-    call putstr
-
-    mov rdi, 0ABCh
-    call puthex
+    call printer
 
     mov rdi, 0x0A
     call putchar
@@ -238,8 +504,4 @@ _start:
 
 section .data
 
-Msg:    db "__Hllwrld", 0x0a
-MsgLen  equ $ - Msg
 TestPutstr: db "putstr test", 0x0A, 0
-Int64MinValue: db "9223372036854775808"
-Int64MinValueLen equ $ - Int64MinValue
--- readme.md
+++ readme.md
@@ -11,10 +11,10 @@ void putchar(char a);
 void putstr(char* str);
 
 // помещает int
-void putint(int a);
+void putint(int64_t a);
 
-// помещает int
-void putuint(int a);
+// помещает uint
+void putuint(uint64_t a);
 
 // помещает число в 16ричном виде
 void puthex(uint64_t a);
@@ -25,67 +25,68 @@ void putoct(uint64_t a);
 // помещает число в 2ичном виде
 void putbin(uint64_t a);
 
-int32_t printf_inter(char *fmt, ...) {
 
-    // fmt stay in rbx
+/* int32_t printer(char *fmt, ...)
+; calling convention:
+;   rdi -> fmt
+;   rsi -> ptr_to_arg1..5
+;   rdx -> ptr_to_arg6...
+;   who pushed - that popped (calling func need to pop all )
+; Почему так?
+; Для более удобного написания трамплина для вызова по ABI. Потому что первые 6 аргументов (без fmt получается 5)
+; лежат в регистрах, а последующие в стеке (под адресом возврата). Поэтому трамплин пушит 5 аргументов в стек, и
+; передает 2 указателя: на 1й аргумент (последний запушенный в стек) и на первый элемент стека трамплина
+;
+; Структура стека:
+;
+*/ | BP | RetAddr | %1 | %2 | %3 | %4 | %5 | aligning | chain jmp BP | chain jmp RetAddr | %6 | ...
 
-asm(
-    push bp
-    mov bp, sp
-)
+int32_t printer(char *fmt, uint8_t *ptr1, uint8_t *ptr2) {
 
-    _bp -= 8; // [sp] - указывает на адрес возврата, перед ним лежит первый аргумент
+    int32_t arg_count = 0;
 
-    int32_t put_count = 0; // local var, stay in eax (return value)
-    while(*fmt != '\0') {
-        if (*fmt == '%') {
+    while (*fmt != '\0') {
+        if (*fmt != '%') {
+            putchar(*fmt);
             ++fmt;
-            switch(*fmt) {
-                case 'b':
-                    putbin(_bp);
-                    _bp -= 8;
-                    break;
-                case 'c':
-                    putchar(_bp);
-                    _bp -= 1;
-                    break;
-                case 'd':
-                    putint(_bp);
-                    _bp -= 8;
-                    break;
-                case 'o':
-                    putoct(_bp);
-                    _bp -= 8;
-                    break;
-                case 's':
-                    putstr(_bp);
-                    _bp -= 8;
-                    break;
-                case 'u':
-                    putuint(_bp);
-                    _bp -= 8;
-                    break;
-                case 'x':
-                    putoct(_bp);
-                    _bp -= 8;
-                    break;
-                default:
-                    // UB
-                    asm (jmp 0)
-            }
-            ++fmt;
-        } else {
-            putchar(buf, *fmt);
-            ++put_count;
-            ++fmt;
+            continue;
         }
+
+        ++fmt;
+
+        if (*fmt == '\0') {
+            // UB
+            break;
+        }
+
+        if (*fmt == '%') {
+            putchar('%');
+            ++fmt;
+            continue;
+        }
+
+        uint8_t *arg_ptr;
+        if (arg_count < 5)
+            arg_ptr = ptr1 + arg_count * 8;
+        else
+            arg_ptr = ptr2 + (arg_count - 5) * 8;
+
+        switch (*fmt) {
+            case 'b': putbin(arg_ptr);   break;
+            case 'c': putchar(*arg_ptr); break;
+            case 'd': putint(arg_ptr);   break;
+            case 'o': putoct(arg_ptr);   break;
+            case 's': putstr(arg_ptr);   break;
+            case 'u': putuint(arg_ptr);  break;
+            case 'x': puthex(arg_ptr);   break;
+            default:
+                // UB
+                break;
+        }
+
+        ++arg_count;
+        ++fmt;
     }
-
-asm(
-    pop bp
-    ret
-)
-
 }
 
 ```
